bf07ed384c0c4c8dc1cb0d56e46a7fa10737c056
“kurabunoyama” <jsnider@stripe.com>
2019-04-23 16:55:28 -0700

    dancing queen

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ function purchaseProduct(event) {
   alert('hello world');
 }
 
-function openProductModal() {
+function openProductModal(id) {
+  let product = document.getElementById(id)
+  let title = product.dataset.title
+  let price = product.dataset.price
+  let description = product.dataset.description
+  let modalTitle = document.getElementById('productItemTitle')
+  let modalDescription = document.getElementById('productItemDescription')
+  let modalPrice = document.getElementById('productItemPrice')
+  modalTitle.innerHTML = title
+  modalDescription.innerHTML = description
+  productItemPrice.innerHTML = price
+  console.log(product.dataset.price)
   $('#purchaseProductModal').modal('show')
 }
+
